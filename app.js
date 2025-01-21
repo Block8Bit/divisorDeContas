a86c95b4118a2b32d604d9a9c179704b948628c9
Block8Bit <lucasoaresnogueira2004@gmail.com>
2025-01-21 11:22:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function calcular() {
+  let salario1 = parseFloat(document.querySelector("#salario1").value);
+  let salario2 = parseFloat(document.querySelector("#salario2").value);
+  let salarioTotal = salario1 + salario2;
+  let conta = parseFloat(document.querySelector("#conta").value);
+  let pagamento1 = Math.round((salario1 / salarioTotal) * conta * 100) / 100;
+  let pagamento2 = Math.round((salario2 / salarioTotal) * conta * 100) / 100;
+
+  console.log(
+    `Lucas tem que pagar ${pagamento1} e Luiz tem que pagar ${pagamento2}`
+  );
+
+  let mensagem = document.querySelector("h2");
+  mensagem.textContent = `A primeira pessoa paga R$${pagamento1} e a segunda paga R$${pagamento2}.`;
+}
